4a3a344aca4d895d76335087540a13bdb88f42cc
Supratik <rikc.2000@gmail.com>
2021-02-03 13:14:55 +0530

    adding server files

---
+++ controllers/posts.js
@@ -0,0 +1,62 @@
+import PostMessage from "../models/postMessage.js";
+import mongoose from "mongoose";
+
+export const getPost = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+        // console.log(postMessages);
+        res.status(200).json(postMessages);
+    
+    } catch(err) {
+        res.status(404).json({message: err.message}); 
+    }
+}
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post); 
+
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch(err) {
+        res.status(409).json({message: err.message}); 
+    }
+}
+
+export const updatePost = async (req, res) => {
+    const {id : _id} = req.params;
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id))
+        return res.status(404).send("No post with that id");
+
+    // updated post will have new _id so change id    {...post, _id}
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, {new: true})
+
+    res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+    const {id: _id} = req.params;
+    
+    if(!mongoose.Types.ObjectId.isValid(_id))
+        return res.status(404).send("No post with that id");
+
+    await PostMessage.findByIdAndRemove(_id);
+    res.json({message: "Post deleted successfully"});
+
+}
+
+export const likePost = async (req, res) => {
+    const {id : _id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(_id))
+        return res.status(404).send("No post with that id");
+
+    const post = await PostMessage.findById(_id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {likeCount: post.likeCount+1}, {new: true});
+
+    res.json(updatedPost);
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+import postRoutes from "./routes/posts.js"; 
+
+const app = express()
+dotenv.config()
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+const PORT = process.env.port || 5000;
+
+// mongoose.set("useFindAndModify", true);
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+    .catch((err) => console.log(err.message))
+
+
+
+
+

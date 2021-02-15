1f61388bd2c48e55b279d9c2946f3b500b632d9a
Supratik <rikc.2000@gmail.com>
2021-02-15 16:28:54 +0530

    jwt done

--- controllers/posts.js
+++ controllers/posts.js
@@ -15,10 +15,11 @@ export const getPost = async (req, res) => {
 export const createPost = async (req, res) => {
     const post = req.body;
 
-    const newPost = new PostMessage(post); 
+    const newPost = new PostMessage({...post, creator: req.userId, createdAt: new Date().toISOString()}); 
 
     try {
         await newPost.save();
+        console.log(`post created by ${req.userId}`);
         res.status(201).json(newPost);
     } catch(err) {
         res.status(409).json({message: err.message}); 
@@ -51,12 +52,27 @@ export const deletePost = async (req, res) => {
 
 export const likePost = async (req, res) => {
     const {id : _id} = req.params;
+    // console.log(req.userId);
+
+    if(!req.userId)
+        return res.json({message: "user unauthenticated."})
 
     if(!mongoose.Types.ObjectId.isValid(_id))
         return res.status(404).send("No post with that id");
 
     const post = await PostMessage.findById(_id);
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {likeCount: post.likeCount+1}, {new: true});
+
+    const index = post.likes.findIndex((id) => id === String(req.userId));
+
+    if(index === -1) {
+        //like the post
+        post.likes.push(req.userId)
+    } else {
+        // dislike the post
+        post.likes = post.likes.filter((id) => id !== String(req.userId));
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new: true});
 
     res.json(updatedPost);
 }
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv"
 
 import postRoutes from "./routes/posts.js"; 
+import userRoutes from "./routes/user.js"; 
 
 const app = express()
 dotenv.config()
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.use("/user", userRoutes);
 
 const PORT = process.env.port || 5000;
 
